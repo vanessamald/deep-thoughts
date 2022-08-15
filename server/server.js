e195b07e14f47af39b0f8e401531a74b61d203ed
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-08-15 11:57:03 -0500

    initial file setup, installing apollo, connecting Apollo to express server

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+// import Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// create a new instance of an Apollo server with the GraphQL schema 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+// integrate Apollo Server with Express as middleware
+server.applyMiddleware({ app });
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
+}
+
+// call the async function to start the server
+startApolloServer(typeDefs, resolvers);
